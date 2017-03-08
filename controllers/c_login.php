4cb4fcfae47e48243c21607ac68202cc08560959
Aditya Kamath <a_kamath@hotmail.com>
2017-03-08 18:05:29 +0000

    Finish login

--- controllers/c_login.php
+++ controllers/c_login.php
@@ -1,4 +1,54 @@
 <?php
     require("../controllers/includes/helpers.php");
-    render("v_login.html", array("title" => "Login"));
+    require("../models/m_login.php");
+    
+    // Normal page load
+    if($_SESSION["REQUEST_METHOD"] == "GET")
+        render("v_login.html", array("title" => "Login"));
+        
+    // Submit info
+    else if($_SESSION["REQUEST_METHOD"] == "POST")
+    {
+        // Check for valid inputs
+        if(empty($_POST["email"]) || empty($_POST["password"]))
+        {
+            render("v_login.html", array("title" => "Login", "msg" => "Email and Password required."));
+            exit;
+        }
+        
+        // Check proper format for email
+        if(!preg_match("/\S+@\S+\.\S+/", $_POST["email"]))
+        {
+            render("v_login.html", array("title" => "Login", "msg" => "Invalid Email."));
+            exit;
+        }
+        
+        // Request DB info
+        $details = check($_POST);
+        
+        // User not found
+        if(empty($details))
+        {
+            render("v_login.html", array("title" => "Login", "msg" => "User not found."));
+            exit;
+        }
+        
+        // Password does not match
+        if($details == -1)
+        {
+            render("v_login.html", array("title" => "Login", "msg" => "Invalid password."));
+            exit;
+        }
+        
+        // Everything looks good, let's get this show on the road
+        else
+        {
+            $_SESSION["id"]    = $details["id"];
+            $_SESSION["name"]  = $details["name"];
+            $_SESSION["sex"]   = $details["sex"];
+            $_SESSION["email"] = $details["email"];
+            header("Location: /index.php");
+            exit;
+        }
+    }
 ?>
--- models/m_login.php
+++ models/m_login.php
@@ -1,4 +1,20 @@
 <?php
     require("../models/mysql.php");
-    
+    function check($data)
+    {
+        $connection = connect("../models/config.json");
+        $rows = mysqli_fetch_row(mysqli_query($connection, "SELECT Id, Name, Gender, Password FROM Users WHERE Email = \"" . $data["email"] . "\";"));
+        if(empty($rows))
+            return 0;
+        else if(!password_verify($data["password"], $rows[3]))
+            return -1;
+            
+        $data = array(
+            "id" => $rows[0],
+            "name" => $rows[1],
+            "sex" => $rows[2],
+            "email" => $data["email"]
+            );
+        return $data;
+    }
 ?>
