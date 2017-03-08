8ac141978ae0d7dbb8583bf9ec58611ae04a7539
Ronit Hire <ronit.hire@gmail.com>
2017-03-08 10:05:48 +0000

    Front-end basic edits

--- public/js/jquery.js
+++
@@ -1,5 +0,0 @@
-/**
- * jquery.js
- *
- * Global JavaScript.
- */
--- views/header.php
+++ views/header.php
@@ -1,9 +1,28 @@
 <!DOCTYPE html>
 <html>
     <head>
+        
+        <meta charset = "utf-8">
+        <meta name = "viewport" content="width=device-width, initial-scale=1">
+        
         <title><?= htmlspecialchars($title) ?></title>
-        <link rel=stylesheet type="text/css" href="/css/styles.css">
-		<script src="js/jquery.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="/css/styles.css">
+		
+		 <!-- http://jquery.com/ -->
+        <script src="js/jquery-1.11.3.min.js"></script>
+		
+		<!-- http://underscorejs.org/ -->
+        <script src="js/underscore-min.js"></script>
+        
+        <!--Might remove it later-- https://github.com/twitter/typeahead.js/ -->
+        <script src="js/typeahead.jquery.min.js"></script>
+		
     </head>
     <body>
         <div class="container">
+            <div id="top">
+                <div> Some title with some image </div>
+            </div>
+            
+            <div id="middle">
