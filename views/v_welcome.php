638929e75103526408b7ff03cd7442556e52448c
Aditya Kamath <a_kamath@hotmail.com>
2017-03-10 18:31:59 +0000

    Part of table on user page

--- views/v_welcome.php
+++ views/v_welcome.php
@@ -2,3 +2,30 @@
 <a href='/postad.php'>Sell Item</a><br>
 <a href='/store.php'>Go to Store</a><br>
 <a href='/logout.php'>Logout</a>
+<?php if(!empty($data)): ?>
+    <form method=POST action='/'>
+    <table style="border: 2px solid wheat;">
+    	<tbody>
+    	    <tr>
+        		<th>Image</th>
+        		<th>Title</th>
+        		<th>Description</th>
+        		<th>Price</th>
+        		<th>Date</th>
+        		<th>Remove</th>
+    		</tr>
+    		
+    		    <?php foreach ($data as $obj): ?>
+        		<tr>
+    				<td><img width="100px" height="50px" src= "<?php echo "images/" . $obj["image"]; ?>"></td>
+    				<td><?= $obj["name"]; ?></td>
+    				<td><?= $obj["desc"]; ?></td>
+    				<td><?= $obj["price"]; ?></td>
+    				<td><?= $obj["date"]; ?></td>
+                    <td><button name="delete" value= <?= $obj["item"]; ?>>Delete</button></td>
+    			</tr>
+			<?php endforeach ?>
+		</tbody>
+	</table>
+	</form>
+<?php endif; ?>
